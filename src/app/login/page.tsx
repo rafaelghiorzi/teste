57487c71ceb658d9811fe86acda5c608f188c8e8
rafaelghiorzi <rafael.ghiorzi@gmail.com>
2024-05-13 22:16:11 -0300

    foda

---
+++ src/app/login/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <main className="text-black bg-gray-200 flex">tela de login</main>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
-import TelaDeLogin from "./components/teladelogin";
-
 export default function Home() {
   return (
     <main className="text-black bg-gray-200 flex">
-      <TelaDeLogin link="https://instagram.com" />
-      <TelaDeLogin link="https://youtube.com" />
+      <img src="/fundo.png" alt="foto_de_fundo" className="w-1/2 h-screen" />
+      <section className="flex-col justify-center w-1/2 items-center flex p-32">
+        <h1 className="mb-20 text-3xl font-semibold">
+          Avaliação de professores
+        </h1>
+        <input className="p-2 m-3 rounded-lg w-full" type="text" placeholder="nome" />
+        <input className="p-2 m-3 rounded-lg w-full" type="text" placeholder="senha" />
+        <div className="flex flex-row">
+          <button className="p-2 m-3 bg-green-300 rounded-lg">entrar</button>
+          <button className="p-2 m-3 bg-green-300 rounded-lg">Criar conta</button>
+        </div>
+      </section>
     </main>
   );
 }
